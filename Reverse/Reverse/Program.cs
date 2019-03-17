c3828cc68b0ebf4d0cf54e69463625b809ddcca8
Mads Kildegaard Kristjansen <m.i.s.s.mads@hotmail.com>
2019-03-17 21:41:55 +0100

    Done 101

--- Reverse/Reverse/Program.cs
+++ Reverse/Reverse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace Reverse
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to Reverse Game");
+            Console.WriteLine("The rules are simple. You type 3 numbers. I give you back the 3 numbers in reverse order.");
+            Console.WriteLine("Please give me your first number");
+            string firstNumber = Console.ReadLine();
+            int firstNumberFuture = int.Parse(firstNumber);
+            Console.WriteLine("Good Please give me your second number");
+            string secondNumber = Console.ReadLine();
+            int secondNumberFuture = int.Parse(secondNumber);
+            Console.WriteLine("Good Please give me your third number");
+            string thirdNumber = Console.ReadLine();
+            int thirdNumberFuture = int.Parse(thirdNumber);
+            Console.WriteLine("");
+            Console.WriteLine("Your numbers reverse is:");
+
+            Stack myStack = new Stack();
+            myStack.Push(firstNumberFuture);
+            myStack.Push(secondNumberFuture);
+            myStack.Push(thirdNumberFuture);
+
+            // Displays the properties and values of the Stack.
+            PrintValues(myStack);
+            Console.ReadLine();
+        }
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("{0}", obj);
+            Console.WriteLine();
         }
     }
 }
